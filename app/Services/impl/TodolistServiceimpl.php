9aa1642706f98c309965cfda7058f5db3af17a08
JODI ADRIAN <jodiadrian15@gmail.com>
2025-05-30 17:20:39 +0700

    `Membuat Logic Todo
    menambah logic untuk menambah pada todolist

--- app/Services/impl/TodolistServiceimpl.php
+++ app/Services/impl/TodolistServiceimpl.php
@@ -3,8 +3,19 @@
 namespace App\Services\impl;
 
 use App\Services\TodolistService;
+use Illuminate\Support\Facades\Session;
 
 class TodolistServiceimpl implements TodolistService
 {
+    public function saveTodo(string $id, string $todo): void
+    {
+        if(!Session::exists("todolist")) {
+            Session::put("todolist". []);
+        }
 
+        Session::push("todolist", [
+            "id" => $id,
+            "todo" => $todo
+        ]);
+    }
 }
--- tests/Feature/TodolistServiceTest.php
+++ tests/Feature/TodolistServiceTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Services\TodolistService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 
 class TodolistServiceTest extends TestCase
@@ -21,5 +22,16 @@ class TodolistServiceTest extends TestCase
    public function testTodolistServiceNotNull()
    {
        self::assertNotNull($this->todolistService);
+   }
+
+   public function testsaveTodo()
+   {
+       $this->todolistService->saveTodo("1", "jodi");
+
+       $todolist = Session::get("todolist");
+       foreach ($todolist as $value) {
+           self::assertEquals("1", $value['id']);
+           self::assertEquals("jodi", $value['todo']);
+       }
    }
 }
